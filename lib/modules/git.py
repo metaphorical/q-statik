0f282af6c6dbb805ccefdb1c1b6bc65c0d0635f2
Rastko <b.metaphorical@gmail.com>
2015-11-01 22:11:58 +0100

    Adding systematic stuff before solution happening

--- lib/modules/git.py
+++ lib/modules/git.py
@@ -1,11 +1,15 @@
 import subprocess
-
+from lib.modules.mutils import which
 
 class GitControl(object):
     def __init__(self, repo_config):
         self._repoDir = repo_config.dir if (dir in repo_config) else False
         self._remoteUrl = repo_config.url
         self._remoteName = repo_config.remote
+        if which('git') is None:
+            raise ValueError('Git needs to be installed in the sysem...')
+
+        raise ValueError('Parameter should...')
 
     def add(self, add_string):
         cmd = ['git', 'add', add_string]
@@ -21,5 +25,10 @@ class GitControl(object):
 
     def status(self):
         cmd = ['git', 'status']
+        p = subprocess.Popen(cmd, cwd=self._repoDir)
+        p.wait()
+
+    def co(self, co_string):
+        cmd = ['git', 'checkout', co_string]
         p = subprocess.Popen(cmd, cwd=self._repoDir)
         p.wait()
